156c2e6a7e99ca46e005295527493b07a4bf39bb
MariBelniok <mariannabelniok@outlook.com>
2022-01-30 21:25:25 -0300

    ajuste

--- src/app/shared/directives/string-color-change.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { StringColorChangeDirective } from './string-color-change.directive';
-
-describe('StringColorChangeDirective', () => {
-  it('should create an instance', () => {
-    const directive = new StringColorChangeDirective();
-    expect(directive).toBeTruthy();
-  });
-});
