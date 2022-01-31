9598079afce7af15a815a2996c19816d3b10fc3b
MariBelniok <mariannabelniok@outlook.com>
2022-01-30 21:13:15 -0300

    colors working

---
+++ src/app/shared/directives/string-color-change.directive.spec.ts
@@ -0,0 +1,8 @@
+import { StringColorChangeDirective } from './string-color-change.directive';
+
+describe('StringColorChangeDirective', () => {
+  it('should create an instance', () => {
+    const directive = new StringColorChangeDirective();
+    expect(directive).toBeTruthy();
+  });
+});
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InformationDialogComponent } from '../shared/dialog/information-dialog.component';
 import { MatDialogModule } from '@angular/material/dialog';
-
+import { ColorInputDirective } from './directives/string-color-change.directive'
 @NgModule({
   declarations: [
-    InformationDialogComponent
+    InformationDialogComponent,
+    ColorInputDirective
   ],
   imports: [
     CommonModule,
@@ -13,7 +14,8 @@ import { MatDialogModule } from '@angular/material/dialog';
   ],
   exports: [
     InformationDialogComponent,
-    MatDialogModule
+    MatDialogModule,
+    ColorInputDirective
   ]
 })
 
