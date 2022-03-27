ca219d89f6031e305256c3b344ab53873773fc19
maribelniok <mariannabelniok@outlook.com>
2022-03-27 11:21:15 -0300

    game container service test done

--- src/app/game-container/game-container.service.spec.ts
+++ src/app/game-container/game-container.service.spec.ts
@@ -1,16 +1,69 @@
 import { TestBed } from '@angular/core/testing';
-
 import { GameContainerService } from './game-container.service';
 
-describe('GameContainerService', () => {
-  let service: GameContainerService;
+fdescribe('GameContainerService', () => {
+  let serviceSpy: jasmine.SpyObj<GameContainerService>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(GameContainerService);
+    TestBed.configureTestingModule({
+      providers: [GameContainerService]
+    });
+    serviceSpy = TestBed.inject(GameContainerService) as jasmine.SpyObj<GameContainerService>;
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(serviceSpy).toBeTruthy();
   });
+
+  it('should set players ready', () => {
+    let playersReady = false;
+    serviceSpy.setPlayersReady();
+
+    serviceSpy.playersReady$.subscribe(value => playersReady = value);
+
+    expect(playersReady).toBeTruthy();
+  })
+
+  it('should be playerX turn', () => {
+    let isPlayerXTurn = false;
+    serviceSpy.setIsPlayerXTurn(true);
+    serviceSpy.isPlayerXTurn$.subscribe(value => isPlayerXTurn = value);
+
+    expect(isPlayerXTurn).toBeTruthy();
+  })
+
+  it('should not be to playerX turn', () => {
+    let isPlayerXTurn = false;
+    serviceSpy.setIsPlayerXTurn(false);
+    serviceSpy.isPlayerXTurn$.subscribe(value => isPlayerXTurn = value);
+
+    expect(isPlayerXTurn).not.toBeTruthy();
+  })
+
+  it('should increase playerX score when it wins a match', () => {
+    let playerX = 0;
+    serviceSpy.roundWinner('X');
+
+    serviceSpy.gameScore$.subscribe(value => playerX = value.playerX);
+
+    expect(playerX).toBe(1);
+  })
+
+  it('should increase playerO score when it wins a match', () => {
+    let playerO = 0;
+    serviceSpy.roundWinner('O');
+
+    serviceSpy.gameScore$.subscribe(value => playerO = value.playerO);
+
+    expect(playerO).toBe(1);
+  })
+
+  it('should increase tie score when match has no winner', () => {
+    let tie = 0;
+    serviceSpy.roundWinner('Tie');
+
+    serviceSpy.gameScore$.subscribe(value => tie = value.tie);
+
+    expect(tie).toBe(1);
+  })
 });
