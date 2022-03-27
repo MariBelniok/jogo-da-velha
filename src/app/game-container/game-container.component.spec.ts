f1d99b84c8280ef58bfa98e7dd0019dbd3d8829e
maribelniok <mariannabelniok@outlook.com>
2022-03-27 11:33:21 -0300

    game container component test done

--- src/app/game-container/game-container.component.spec.ts
+++ src/app/game-container/game-container.component.spec.ts
@@ -1,14 +1,28 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 
 import { GameContainerComponent } from './game-container.component';
+import { GameContainerService } from './game-container.service';
+import { GameScoreModel } from './models/game-score.model';
 
-describe('GameContainerComponent', () => {
+const gameContainerServiceMock = {
+  gameScore$: of({
+    playerX: 2,
+    playerO: 1,
+    tie: 3
+  }),
+}
+
+fdescribe('GameContainerComponent', () => {
   let component: GameContainerComponent;
   let fixture: ComponentFixture<GameContainerComponent>;
+  let service: GameContainerService;
 
   beforeEach(async () => {
+
     await TestBed.configureTestingModule({
-      declarations: [ GameContainerComponent ]
+      declarations: [ GameContainerComponent ],
+      providers: [{ provide: GameContainerService, useValue: gameContainerServiceMock}]
     })
     .compileComponents();
   });
@@ -16,10 +30,20 @@ describe('GameContainerComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(GameContainerComponent);
     component = fixture.componentInstance;
+    service = TestBed.inject(GameContainerService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should request game score on component initiation', async () => {
+    fixture.detectChanges();
+    await fixture.whenStable().then(() => {
+      expect(component.gameScore.playerX).toBe(2);
+      expect(component.gameScore.playerO).toBe(1);
+      expect(component.gameScore.tie).toBe(3);
+    })
   });
 });
--- src/app/game-container/game-container.service.spec.ts
+++ src/app/game-container/game-container.service.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { GameContainerService } from './game-container.service';
 
-fdescribe('GameContainerService', () => {
+describe('GameContainerService', () => {
   let serviceSpy: jasmine.SpyObj<GameContainerService>;
 
   beforeEach(() => {
