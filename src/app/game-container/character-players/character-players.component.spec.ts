f229e3b4c8e15e71c54975bc322e12814e85a29d
maribelniok <mariannabelniok@outlook.com>
2022-03-29 07:40:18 -0300

    characters players test done

--- src/app/game-container/character-players/character-players.component.spec.ts
+++ src/app/game-container/character-players/character-players.component.spec.ts
@@ -1,14 +1,44 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogModule } from '@angular/material/dialog';
+import { of } from 'rxjs';
+import { GameContainerService } from '../game-container.service';
+import { CharacterModel } from '../models/character.model';
 
 import { CharacterPlayersComponent } from './character-players.component';
+import { CharacterPlayersService } from './characters-players.service';
+
+const characterPlayersServiceMock = {
+  playerCharacters$: of([
+    {
+      player: 'X',
+      name: 'Thor',
+      thumbnailUrl: 'thor.png'
+    },
+    {
+      player: 'O',
+      name: 'Hulk',
+      thumbnailUrl: 'hulk.png'
+    }
+  ]),
+  isPlayerXTurn$: true,
+  setPlayersReady: () => { return true }
+}
 
 describe('CharacterPlayersComponent', () => {
   let component: CharacterPlayersComponent;
   let fixture: ComponentFixture<CharacterPlayersComponent>;
+  let containerService: GameContainerService;
+  let charactersService: CharacterPlayersService
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CharacterPlayersComponent ]
+      declarations: [ CharacterPlayersComponent ],
+      imports: [HttpClientTestingModule, MatDialogModule],
+      providers: [
+        GameContainerService,
+        { provide: CharacterPlayersService, useValue: characterPlayersServiceMock }
+      ]
     })
     .compileComponents();
   });
@@ -17,9 +47,40 @@ describe('CharacterPlayersComponent', () => {
     fixture = TestBed.createComponent(CharacterPlayersComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    containerService = TestBed.inject(GameContainerService);
+    charactersService = TestBed.inject(CharacterPlayersService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should load two characters on init', async () => {
+    let players = [];
+    fixture.detectChanges();
+    await fixture.whenStable().then(() => {
+      charactersService.playerCharacters$.subscribe(value => players = value);
+      expect(players.length).toBe(2);
+    });
+  });
+
+  it('should change isXTurn to true when init', async () => {
+    let isXTurn = false;
+    fixture.detectChanges();
+    await fixture.whenStable().then(() => {
+      containerService.isPlayerXTurn$.subscribe(value => isXTurn = value);
+      expect(component.isXturn).toBeTruthy();
+    });
+  });
+
+  it('should set players ready on init', async () => {
+    fixture.detectChanges();
+    await fixture.whenStable().then(() => {
+      charactersService.playerCharacters$.subscribe(value => {
+        if (value.length === 2) {
+          expect(characterPlayersServiceMock.setPlayersReady()).toHaveBeenCalled();
+        }
+      });
+    });
+  })
 });
