0f440859f0a8af9598b5f6b29b81a2e115cf46ea
maribelniok <mariannabelniok@outlook.com>
2022-04-01 18:08:41 -0300

    game-board tests done

--- src/app/game-container/game-board/game-board.component.spec.ts
+++ src/app/game-container/game-board/game-board.component.spec.ts
@@ -24,4 +24,58 @@ fdescribe('GameBoardComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should mark space with X or O', () => {
+    const spy = spyOn(component, 'markSpace');
+    const space = fixture.nativeElement.querySelector('.space');
+    space.click();
+
+    fixture.detectChanges();
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should have a winner when possible index combinations matches', () => {
+    component.currentPlayerIsX = false;
+    component.gameBoardSpaces = ['X', 'X', 'X'];
+
+    const space = fixture.nativeElement.querySelector('.space');
+    space.click();
+
+    fixture.detectChanges();
+
+    expect(component.winner).toBe('X');
+  });
+
+  it('should show winner', () => {
+    component.currentPlayerIsX = false;
+    component.gameBoardSpaces = ['X', 'X', 'X'];
+    const space = fixture.nativeElement.querySelector('.space');
+    space.click();
+
+    fixture.detectChanges();
+
+    const winner = fixture.nativeElement.querySelector('.showWinner');
+
+    expect(component.winner).toBe('X');
+    expect(winner).toBeDefined();
+  });
+
+
+  it('should show tie', () => {
+    component.movesCounter = 9;
+    component.gameBoardSpaces = [
+      'X', 'O', 'X',
+      'O', 'O', 'X',
+      'X', 'X', 'O'
+    ];
+    const space = fixture.nativeElement.querySelector('.space');
+    space.click();
+
+    fixture.detectChanges();
+
+    const winner = fixture.nativeElement.querySelector('.showWinner');
+
+    expect(component.winner).toBe('Tie');
+    expect(winner).toBeDefined();
+  });
 });
